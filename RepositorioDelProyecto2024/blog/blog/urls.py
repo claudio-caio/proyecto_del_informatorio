72691f12b10dd900fd7015d3ce41328f08776e54
Denguesito <denguesito2013@gmai.com>
2024-10-03 23:47:20 -0300

    creacion app usuario

--- RepositorioDelProyecto2024/blog/blog/urls.py
+++ RepositorioDelProyecto2024/blog/blog/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('nosotros', views.Nosotros, name='nosotros'),
     path('emprendimientos/', include(('apps.emprendimientos.urls', 'emprendimientos'), namespace='emprendimientos')),
     path('comentarios/', include(('apps.comentarios.urls', 'comentarios'), namespace='comentarios')),
+    path('Usuarios/', include(('apps.Usuarios.urls', 'Usuarios'), namespace='Usuarios')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
